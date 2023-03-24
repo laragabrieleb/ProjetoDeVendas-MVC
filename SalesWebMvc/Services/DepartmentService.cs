bb7d6dbc9dec9493657fd89f351f49760f24a243
Lara Gabriele <laragboliver@gmail.com>
2023-03-24 16:34:06 -0300

    Update seller and custom service exception

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using SalesWebMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -16,6 +18,11 @@ namespace SalesWebMvc.Services
         public List<Department> FindAll()
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+        internal void Update(Seller seller)
+        {
+            throw new NotFoundException("Id not found");
         }
     }
 }
